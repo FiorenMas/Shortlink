34768641f176b16116cd680e552af6f7e0519752
yue374 <102145692+FiorenMas@users.noreply.github.com>
2023-12-09 14:49:06 +0700

    Update editor.sh

--- editor.sh
+++ editor.sh
@@ -11,7 +11,7 @@ file1_content=${file1_content//"// @updateURL https://update.greasyfork.org/scri
 echo "$file1_content" > ./release/ShortLink1-modified.user.js
 file2_content=$(cat base2.user.js)
 file2_content=${file2_content//"function redirect(url, blog = true) {location = blog ? 'https://rotator.nurul-huda.sch.id/?BypassResults=' + url : url;}"/"function redirect(url, blog = true) {location = blog ? '' + url : url;}"}
-file2_content=${file2_content//"// @downloadURL https://update.greasyfork.org/scripts/443888/Additional%20Bypass.meta.js"/"// @downloadURL https://github.com/FiorenMas/Shortlink/releases/download/all/ShortLink2-modified.user.js"}
+file2_content=${file2_content//"// @downloadURL https://update.greasyfork.org/scripts/443888/Additional%20Bypass.user.js"/"// @downloadURL https://github.com/FiorenMas/Shortlink/releases/download/all/ShortLink2-modified.user.js"}
 file2_content=${file2_content//"// @updateURL https://update.greasyfork.org/scripts/443888/Additional%20Bypass.meta.js"/"// @updateURL https://github.com/FiorenMas/Shortlink/releases/download/all/ShortLink2-modified.user.js"}
 echo "$file2_content" > ./release/ShortLink2-modified.user.js
 echo -e "\e[32mThe file has been modified and saved\e[0m"

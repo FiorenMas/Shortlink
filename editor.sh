b7377de63d4a22ae7fd97a307f67ac9cdb416d49
Fioren <102145692+FiorenMas@users.noreply.github.com>
2023-12-22 09:49:56 +0700

    Update editor.sh

--- editor.sh
+++ editor.sh
@@ -6,8 +6,8 @@ curl -o base2.user.js https://update.greasyfork.org/scripts/443888/Additional%20
 curl -o base3.user.js https://openuserjs.org/install/Bloggerpemula/Bypass_All_Shortlinks_Manual_Captcha.user.js
 
 file1_content=$(cat base1.user.js)
-file1_content=${file1_content//"let $ = window.jQuery;let respect = 'https://free4u.nurul-huda.or.id/?BypassResults=';"/"let $ = window.jQuery;let respect = '';"}
-file1_content=${file1_content//"function redirect(url, blog = true) {location = blog ? 'https://rotator.nurul-huda.sch.id/?BypassResults=' + url : url;}"/"function redirect(url, blog = true) {location = blog ? '' + url : url;}"}
+file1_content=${file1_content//"https://free4u.nurul-huda.or.id/?BypassResults="/""}
+file1_content=${file1_content//"https://rotator.nurul-huda.sch.id/?BypassResults="/""}
 file1_content=${file1_content//"$('a.get-link').text('Bypassed by Bloggerpemula');"/"$('a.get-link').text('');"}
 file1_content=${file1_content//"Thanks for using Bypass All Shortlinks Scripts and for Donations , Regards : Bloggerpemula"/""}
 file1_content=${file1_content//"// @downloadURL https://update.greasyfork.org/scripts/431691/Bypass%20All%20Shortlinks.user.js"/"// @downloadURL https://github.com/$repository/releases/download/all/ShortLink1-modified.user.js"}
@@ -22,8 +22,8 @@ file2_content=${file2_content//"// @updateURL https://update.greasyfork.org/scri
 echo "$file2_content" > ./release/ShortLink2-modified.user.js
 
 file3_content=$(cat base3.user.js)
-file3_content=${file3_content//"let $ = window.jQuery;let respect = 'https://free4u.nurul-huda.or.id/?BypassResults=';"/"let $ = window.jQuery;let respect = '';"}
-file3_content=${file3_content//"function redirect(url, blog = true) {location = blog ? 'https://rotator.nurul-huda.sch.id/?BypassResults=' + url : url;}"/"function redirect(url, blog = true) {location = blog ? '' + url : url;}"}
+file3_content=${file3_content//"https://free4u.nurul-huda.or.id/?BypassResults="/""}
+file3_content=${file3_content//"https://rotator.nurul-huda.sch.id/?BypassResults="/""}
 file3_content=${file3_content//"$('a.get-link').text('Bypassed by Bloggerpemula');"/"$('a.get-link').text('');"}
 file3_content=${file3_content//"Thanks for using Bypass All Shortlinks Scripts and for Donations , Regards : Bloggerpemula"/""}
 file3_content=${file3_content//"// @updateURL   https://openuserjs.org/meta/Bloggerpemula/Bypass_All_Shortlinks_Manual_Captcha.meta.js"/"// @updateURL https://github.com/$repository/releases/download/all/ShortLink3-modified.user.js"}

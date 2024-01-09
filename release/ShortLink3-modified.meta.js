8beb81ac8b0869d13c387f8fa9cb2cad4a338fea
FiorenMas <FiorenMas@users.noreply.github.com>
2024-01-09 01:57:19 +0000

    Bump

---
+++ release/ShortLink3-modified.meta.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        Bypass All Shortlinks Manual Captcha
+// @namespace   Violentmonkey Scripts
+// @author      Bloggerpemula
+// @version     90.2
+// @grant       GM_setValue
+// @grant       GM_getValue
+// @grant       GM_addStyle
+// @run-at      document-start
+// @grant       GM_xmlhttpRequest
+// @grant       window.onurlchange
+// @grant       GM_registerMenuCommand
+// @icon        https://i.ibb.co/qgr0H1n/BASS-Blogger-Pemula.png
+// @copyright   2022+, Bloggerpemula (https://openuserjs.org/users/Bloggerpemula)
+// @license     CC-BY-NC-SA-4.0; https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode
+// @updateURL   https://raw.githubusercontent.com/FiorenMas/Shortlink/release/release/ShortLink3-modified.meta.js
+// @license     GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
+// @require     https://update.greasyfork.org/scripts/439099/1203718/MonkeyConfig%20Modern%20Reloaded.js
+// @description Bypass All Shortlinks Sites Automatically Skips Annoying Link Shorteners , Skip AdFly , Linkvertise and No Annoying Ads, Directly to Your Destination and now Support Auto Downloading Your Files
+// @include /^(https?:\/\/)(.+)?(.+)?(\.(com|net|org|st|win|pro|website|fun|app|network|moe|us|ovh|ninja|top|lol|uk|ru|quest|tv|store|news|lat|red|day|ink|icu|works|ee|cfd|men|sx|cash|si|mx|tk|ca|cf|hair|host|download|vip|cc|io|one|ly|gl|so|at|cm|pub|fm|bid|press|lt|xyz|vn|biz|ac|ai|in|im|id|ir|it|la|lc|eu|de|bz|sh|to|info|co|me|np|click|asia|cloud|br|today|digital|online|space|site|tech|work|wiki|sbs|club|link|live|pw|nl))(\/.*)/
+// @exclude /^(https?:\/\/)(.+)?((advertisingexcel|talkforfitness|rsadnetworkinfo|rsinsuranceinfo|rsfinanceinfo|rssoftwareinfo|rshostinginfo|rseducationinfo|gametechreviewer|vegan4k|phineypet|batmanfactor|techedifier|urlhives|linkhives|github|freeoseocheck|greenenez|wiki-topia|edonmanor|vrtier|whatsapp|gearsadviser|edonmanor|tunebug).com|(thumb8|thumb9|crewbase|crewus|shinchu|shinbhu|ultraten|uniqueten|topcryptoz|allcryptoz|coinsvalue|cookinguide|cryptowidgets|webfreetools|carstopia|makeupguide|carsmania).net|(linksfly|shortsfly|urlsfly|wefly|blog24).me|(greasyfork|openuserjs|adarima|telegram).org|mcrypto.club|misterio.ro|insurancegold.in|coinscap.info)(\/.*)/
+// ==/UserScript==
